4255efe41ce2f7d2da934e921436384c9ab0c7e5
leyuyu <leyuyu@tencent.com>
2026-01-09 12:17:02 +0800

    init

---
+++ ai-skills/src/main/java/cn/net/iset/ai/skills/AiSkillsApplication.java
@@ -0,0 +1,13 @@
+package cn.net.iset.ai.skills;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiSkillsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiSkillsApplication.class, args);
+    }
+
+}
---
+++ ai-skills/src/test/java/cn/net/iset/ai/skills/AiSkillsApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.net.iset.ai.skills;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiSkillsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
